cc934245a2d29b2e0c82aba92005a2e34fb072af
CalderMarshall <cmarshall72@gmail.com>
2018-06-11 03:50:50 -0400

    renders stock data in foreach loop

--- src/components/Pinger.js
+++ src/components/Pinger.js
@@ -1,37 +1,55 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import _ from "lodash";
+const list = [
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        {'ip': '192.168.88.49', 'speed' : '1.5' },
+        ];
 class Pinger extends Component{
     constructor(props){
         super(props);
         this.state = {
             ip: null,
-            ms: null
+            ms: null,
         }
     }
     componentWillMount(){
         var self = this;
         axios.get('http://35.199.33.29:8081/')
             .then(function (response) {
-                console.log("ayy", ßresponse.data);
+                console.log("ayy", response.data);
 
-                self.setState({
-                    ip: response.data["192.168.88.49"],
-                    ms: Object.keys(response.data)
-                });
+                // self.setState({
+                //
+                // });
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
+     makeCard() {
+        let things = [];
+        _.forEach(list, function(value){
+            things.push(<div>
+                <h3>{value.ip}</h3>
+                <h5>{value.speed}</h5>
+            </div>)
+        });
+         return things;
+     }
     render(){
-        console.log(this.state.ms);
-        console.log(this.state.ip);
+        if (this.state.list !== null){
+            console.log(this.state.list);
+        }
         return(
             <div>
-                {this.state.ip}
-                <br/>
-                {this.state.ms}
+                {this.makeCard()}
             </div>
         )
     }

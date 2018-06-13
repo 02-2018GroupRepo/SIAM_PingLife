a01f464b17802ca3648a7633c9348d4b27dffc65
CalderMarshall <cmarshall72@gmail.com>
2018-06-12 23:55:48 -0400

    edits card css, adds firstscanned, fixes three dots

--- src/App.js
+++ src/App.js
@@ -1,48 +1,125 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Pinger from './components/Pinger';
 import axios from "axios";
+
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            ip_list: null
+            ip_list: [
+                {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "iPhone",
+                    devicename: "unknown",
+                    speed: "67 Mbps",
+                    firstscanned: "Tue Jun 11 13:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Windows Computer",
+                    devicename: "unknown",
+                    speed: "45 Mbps",
+                    firstscanned: "Mon Jun 10 13:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Printer",
+                    devicename: "unknown",
+                    speed: "down",
+                    firstscanned: "Sun Jun 9 13:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Windows Computer",
+                    devicename: "unknown",
+                    speed: "112 Mbps",
+                    firstscanned: "Tue Jun 12 10:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Windows Computer",
+                    devicename: "unknown",
+                    speed: "112 Mbps",
+                    firstscanned: "Tue Jun 12 20:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "iPhone",
+                    devicename: "unknown",
+                    speed: "down",
+                    firstscanned: "Tue Jun 12 14:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "iPhone",
+                    devicename: "unknown",
+                    speed: "49 Mbps",
+                    firstscanned: "Tue Jun 12 12:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Windows Computer",
+                    devicename: "unknown",
+                    speed: "78 Mbps",
+                    firstscanned: "Tue Jun 12 11:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "unknown",
+                    devicename: "unknown",
+                    speed: "down",
+                    firstscanned: "Tue Jun 12 09:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "Windows Computer",
+                    devicename: "unknown",
+                    speed: "100 Mbps",
+                    firstscanned: "Tue Jun 12 08:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "iPhone",
+                    devicename: "unknown",
+                    speed: "109 Mbps",
+                    firstscanned: "Tue Jun 12 17:25:07 EDT 2018"
+                }, {
+                    ipaddress: "192.168.88.165",
+                    devicetype: "iPhone",
+                    devicename: "unknown",
+                    speed: "98 Mbps",
+                    firstscanned: "Tue Jun 12 18:25:07 EDT 2018"
+                }
+            ]
         }
     }
-    componentWillMount(){
-        var self = this;
-        axios.get('http://104.198.252.163/getall')
-            .then(function (response) {
-                // console.log("ayy", response.data);
-                if (response.data !== null){
-                    self.setState({
-                        ip_list: response.data
-                    });
-                }
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+    componentWillMount() {
+        // var self = this;
+        // axios.get('http://104.198.252.163/getall')
+        //     .then(function (response) {
+        //         // console.log("ayy", response.data);
+        //         if (response.data !== null){
+        //             self.setState({
+        //                 ip_list: response.data
+        //             });
+        //         }
+        //
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
     }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-            <div className="flex-header" style={style.flexer}>
-                {/*<h4><a href="" target="none">Link 1</a></h4>*/}
-                <h3>IP Speeds</h3>
-                <h3>Graph</h3>
-                <h3>Location</h3>
-                <h3>Detail</h3>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="flex-header" style={style.flexer}>
+                        {/*<h4><a href="" target="none">Link 1</a></h4>*/}
+                        <h3>IP Speeds</h3>
+                        <h3>Graph</h3>
+                        <h3>Location</h3>
+                        <h3>Detail</h3>
+                    </div>
+                </header>
+                <Pinger ip_list={this.state.ip_list}/>
             </div>
-        </header>
-          <Pinger ip_list={this.state.ip_list}/>
-      </div>
-    );
-  }
+        );
+    }
 }
+
 const style = {
     flexer: {
         paddingHorizontal: 10,
--- src/components/Machine.js
+++ src/components/Machine.js
@@ -1,56 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import _ from "lodash";
 import MachineDetail from './MachineDetail'
-class Machine extends Component{
-    constructor(props){
+
+class Machine extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             ip: null,
             ms: null,
         }
     }
-    componentWillMount(){
+
+    componentWillMount() {
 
     }
     makeCard() {
+
         let things = [];
         let icon = null;
-        _.forEach(this.props.ip_list, function(value, key){
-            if (value.devicetype === "Printer"){
+        _.forEach(this.props.ip_list, function (value, key) {
+            if (value.devicetype === "Printer") {
                 icon = "images/printer.png"
             }
-            else if (value.devicetype === 'Windows Computer'){
+            else if (value.devicetype === 'Windows Computer') {
                 icon = "images/computer.png"
             }
-            else if (value.devicetype === "iPhone"){
+            else if (value.devicetype === "iPhone") {
                 icon = "images/iphone.png"
             }
-            else{
+            else {
                 icon = "images/notFound.png"
             }
             things.push(
                 <div key={key} style={style.dataDiv}>
 
-                        <img src={icon} alt="" className="deviceIcon"
-                             style={
-                                 value.speed === "down" ? (
-                                     {
-                                         border:"solid 1.5px red"
-                                     }
-                                 ) : (
-                                     {border:"solid 1.5px green"})
-                             }/>
+                    <img src={icon} alt="" className="deviceIcon"
+                         style={
+                             value.speed === "down" ? (
+                                 {
+                                     border: "solid 1.5px red"
+                                 }
+                             ) : (
+                                 {border: "solid 1.5px green"})
+                         }/>
 
-                    <h4>{"IP  " + value.ipAddress}</h4>
-                    <h4>{"Device Type  " + value.devicetype}</h4>
+                    <h5>{"IP  " + value.ipaddress}</h5>
+                    <div style={{paddingRight: '3vw', paddingLeft: "3vw"}}>
+                        <h4>Device Type <p style={{fontWeight: '300', fontSize: ".8em"}}>{value.devicetype}</p></h4>
+                        <h4>First Scanned <p style={{fontWeight: '300', fontSize: ".8em"}}>{value.firstscanned}</p></h4>
+                    </div>
+
 
                     <div style={{
                         display: "flex",
-                        justifyContent: "space-between"
-                            }}>
-                         <h5>{"Speed " + value.speed}</h5>
+                        justifyContent: "space-between",
+                        alignItems: "center"
+                    }}>
+                        <h5>{"Speed " + value.speed}</h5>
 
-                        <img src = "images/dots.png" style = {{width:"12px"}}/>       
+                        <img  src={"images/dots.png"} style={{maxHeight: "12px"}}/>
                     </div>
 
                 </div>
@@ -58,11 +66,12 @@ class Machine extends Component{
         });
         return things;
     }
-    render(){
-        if (this.state.ip_list !== null){
+
+    render() {
+        if (this.state.ip_list !== null) {
             console.log(this.state.ip_list);
         }
-        return(
+        return (
             <div>
 
                 <div style={style.outer}>
@@ -73,6 +82,7 @@ class Machine extends Component{
         )
     }
 }
+
 const style = {
     dataDiv: {
         color: "black",
@@ -91,8 +101,8 @@ const style = {
 
 
     },
-    iconStyle:{
-        border:"solid 3px green"
+    iconStyle: {
+        border: "solid 3px green"
     }
 };
 export default Machine;
